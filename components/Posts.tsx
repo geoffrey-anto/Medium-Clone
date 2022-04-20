b397a8d648ed44188f92614d76a0a361634f31fd
Geoffrey Anto <91791834+Geoffrey-Anto@users.noreply.github.com>
2022-04-20 19:10:44 +0530

    Removed Uneccesary console logs

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -7,7 +7,6 @@ interface Props {
 }
 
 const Posts = ({ posts }: Props) => {
-  console.log(posts)
   return (
     <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
       {posts.map((post) => (
